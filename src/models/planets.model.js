10793dbf633f53cadd3b6bc5645f25a112bb9af5
Christian Seidler <ch.cseidler@gmail.com>
2023-03-22 22:07:36 -0300

    add csv with planet data

--- src/models/planets.model.js
+++ src/models/planets.model.js
@@ -1,3 +1,34 @@
-const planets = [];
+const { parse } = require('csv-parse');
+const fs = require('fs');
+const { resolve } = require('path');
 
-module.exports = planets;
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+  return planet.koi_disposition === 'CONFIRMED'
+    && +planet.koi_insol > 0.36
+    && +planet.koi_insol < 1.11
+    && +planet.koi_prad < 1.6;
+}
+
+function loadPlanetsData() {
+  return new Promise((res, rej) => {
+    fs.createReadStream('src/data/kepler_data.csv')
+      .pipe(parse({
+        comment: '#',
+        columns: true,
+      }))
+      .on('data', (data) => {
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on('error', (err) => rej(err))
+      .on('end', () => resolve());
+  });
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
--- src/server.js
+++ src/server.js
@@ -2,8 +2,16 @@ const http = require('http');
 
 const app = require('./app');
 
+const { loadPlanetsData } = require('./models/planets.model');
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
+
+async function startServer() {
+  await loadPlanetsData();
+}
+
+startServer();
 
 server.listen(PORT);
