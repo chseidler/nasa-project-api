82bf580120cbada5eb62b04fc617ea68337ba0de
Christian Seidler <ch.cseidler@gmail.com>
2023-03-24 18:51:48 -0300

    validation in post request implemented

--- src/routes/launches/launches.controller.js
+++ src/routes/launches/launches.controller.js
@@ -7,7 +7,21 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
 
+  if (!launch.mission
+      || !launch.rocket
+      || !launch.launchDate
+      || !launch.destination) {
+    return res.status(401).json({
+      error: 'Missing required launch property',
+    });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
+  if (launch.launchDate.toString() === 'Invalid Date') {
+    return res.status(400).json({
+      error: 'Invalid launch date',
+    });
+  }
 
   addNewLaunch(launch);
   return res.status(201).json(launch);
