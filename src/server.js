b1c822fc04beb3893617caae8669e533b9fa117b
Christian Seidler <ch.cseidler@gmail.com>
2023-04-03 22:54:56 -0300

    isolation of layer mongoose

--- src/server.js
+++ src/server.js
@@ -1,30 +1,17 @@
 const http = require('http');
-const mongoose = require('mongoose');
-require('dotenv').config();
 
 const app = require('./app');
-
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = process.env.CONNECTION_STRING;
-
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(MONGO_URL);
+  await mongoConnect();
   await loadPlanetsData();
+  server.listen(PORT);
 }
 
 startServer();
-
-server.listen(PORT);
